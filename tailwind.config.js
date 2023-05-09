997ee7287e69a6e3d21e1b7a71e1776166d6e73b
Jonathan Lee <thenameless0n3@gmail.com>
2023-05-08 22:39:14 -0700

    added google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        'ssp': ["'Source Sans Pro'", "cursive"],
+      },
       animation: {
         'blink-slow': 'blink 1s steps(2) infinite',
         'blink-fast': 'blink 0.5s steps(2) infinite'
