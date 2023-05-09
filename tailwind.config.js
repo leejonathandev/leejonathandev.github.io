1fdac9f0cf12e731d7af43301789400de9bd620c
Jonathan Lee <thenameless0n3@gmail.com>
2023-05-09 03:16:06 -0700

    added fade in for background

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ module.exports = {
       animation: {
         'blink-slow': 'blink 1s steps(2) infinite',
         'blink-fast': 'blink 0.5s steps(2) infinite',
-        'fade-in': 'fadein 0.5s ease-out'
+        'fade-in-and-up': 'fadeinup 0.5s ease-out 0.5s forwards',
+        'fade-in': 'fadein 0.7s ease-out forwards'
       },
 
       keyframes: {
@@ -17,6 +18,14 @@ module.exports = {
           '0%': { opacity: 0 }
         },
         fadein: {
+          '0%': {
+            opacity: 0,
+          },
+          '100%': {
+            opacity: 1,
+          }
+        },
+        fadeinup: {
           '0%': {
             opacity: 0,
             transform: 'translateY(25%)',
