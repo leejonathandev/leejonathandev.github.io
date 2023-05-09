e9d14cf9c6f97f40d4104b18fcf288a4cfab567b
Jonathan Lee <thenameless0n3@gmail.com>
2023-05-08 19:00:48 -0700

    converted styles to tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'blink-slow': 'blink 1s steps(2) infinite',
+        'blink-fast': 'blink 0.5s steps(2) infinite'
+      },
+
+      keyframes: {
+        blink: {
+          '0%': { opacity: 0 }
+        }
+      }
+    },
   },
   plugins: [],
 }
